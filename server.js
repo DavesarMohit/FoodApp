b72ef64786fa3167205e24cd60b9045b4f1fe6c8
Mohit Davesar <mohitdavesar061299@gmail.com>
2021-02-22 16:36:32 +0530

    First commit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo")(session)
+const passport = require("passport")
 
 const url = "mongodb://localhost/menu";
 mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true}
@@ -19,6 +20,8 @@ connection.once("open", () => {
 }).catch(err => {
 	console.log("Connection failed...");
 });
+
+
 
 let mongoStore = new MongoDbStore({
 	             mongooseConnection: connection,
@@ -33,14 +36,22 @@ app.use(session({
 	cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }))
 
+//passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
 	res.locals.session = req.session
+	res.locals.user = req.user
 	next()
 })
 
